910b4ac09a83528966b190f7f007ca861c4e561b
diluu Shree <diluushree@gmail.com>
2025-12-05 22:03:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1931519546-9906b",
+  "appId": "1:913982485204:web:d48b9930662ea334f84141",
+  "apiKey": "AIzaSyDSBPUnijGlz0HgoSq2_JGA11PvvylxJw8",
+  "authDomain": "studio-1931519546-9906b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "913982485204"
+};
